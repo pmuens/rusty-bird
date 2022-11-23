05797ae2dee0735a02b669feea821ba9dd96a9d4
Philipp Muens <philipp@muens.io>
2022-11-23 12:06:55 +0100

    Update shell.nix file

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [ cargo clang gcc libiconv rustc rustup ];
+  buildInputs = with pkgs; [
+    rustup
+    libiconv
+    darwin.apple_sdk.frameworks.AppKit
+    darwin.apple_sdk.frameworks.ApplicationServices
+    darwin.apple_sdk.frameworks.CoreVideo
+    darwin.apple_sdk.frameworks.OpenGL
+  ];
 }
